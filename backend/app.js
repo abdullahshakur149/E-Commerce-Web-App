662d8f15051455f17972378473b26338f5ac47c4
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2024-01-14 19:10:28 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -4,21 +4,21 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// middlewares
+// Middlewares
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(cors()); // Enable CORS for all routes
 
-// config
+// Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
         path: "backend/config/.env" 
     });
 }
 
-// import routes
+// Import routes
 const user = require('./controllers/userController');
 app.use('/api/v2/user', user);
 
