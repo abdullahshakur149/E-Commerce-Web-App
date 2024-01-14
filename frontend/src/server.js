80d437a3034b024d189708d85b58a649dd1490e6
Abdullah Shakur <109961338+abdullahshakur149@users.noreply.github.com>
2024-01-14 20:55:42 +0500

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 // Update the server URL in your frontend code
-export const server = "https://e-commerce-web-app-6orw.vercel.app/api/v2";
+export const server = "https://e-commerce-web-app-v5an.vercel.app/api/v2";
 
 // Update the server URL in your frontend code
-export const backend_url = "https://e-commerce-web-app-6orw.vercel.app/";
+export const backend_url = "https://e-commerce-web-app-v5an.vercel.app/";
